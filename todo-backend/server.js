61d0a3c8ee1e536d5702e1f22dad53e6cc4b29f1
therealkratika <therealkratikagupta@gmail.com>
2025-09-20 17:12:12 +0530

    Update server.js

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -12,7 +12,7 @@ connectDB();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://smart-todo-frontend.onrender.com",
   credentials: true,
 }));
 //route
